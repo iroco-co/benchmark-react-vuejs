bf4186eebb3f87a621b85e213ab42493c15e9725
Lucien Perouze <lucien.perouze@cezembre.co>
2021-02-01 18:11:29 +0100

    Dynamic port

--- api/app.py
+++ api/app.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from mails import mails
+import os
+
 
 app = Flask(__name__)
 app.register_blueprint(mails)
@@ -7,4 +9,4 @@ app.register_blueprint(mails)
 if __name__ == "__main__":
     from waitress import serve
 
-    serve(app, host="0.0.0.0", port=8080)
+    serve(app, host="0.0.0.0", port=os.environ.get('PORT'))
