12a810123e769f8a015da74fb0f1b569d9a5fe78
Lucien Perouze <lucien.perouze@cezembre.co>
2021-02-24 11:47:34 +0100

    API Cors

--- api/app.py
+++ api/app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 from mails import mails
 import os
 
 
 app = Flask(__name__)
+CORS(app)
 app.register_blueprint(mails)
 
 if __name__ == "__main__":
